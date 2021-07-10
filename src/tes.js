70921785bf4d4df9d4ba7db9165308b55d23dd2d
brian <soulsharys@gmail.com>
2021-07-10 15:32:21 +0900

    자바스크립트 연습

--- src/tes.js
+++ src/tes.js
@@ -1,7 +1,3 @@
-// I know this is dumb
-// Just for demo purpose
-export const tes = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve("Hello World"), 2000);
-  });
-}
+
+console.log('tes.js');
+
