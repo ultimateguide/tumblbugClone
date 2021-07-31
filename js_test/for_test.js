4fdcbb2b264bf5687acb37986a5bb24df7ccfda0
brian <soulsharys@gmail.com>
2021-07-31 15:44:41 +0900

    별만들기 +

--- js_test/for_test.js
+++ js_test/for_test.js
@@ -1,15 +1,29 @@
 
 function makeStar(num) {
+  let half = Math.round(num / 2)
+  console.log(half);
+
   for (let i = 1; i <= num; i++) {
     var star = '';
-    for (let j = num; j > 0; j--) {
-      star += i<j ? ' ' : '*'
+    for (let j = 1; j <= num; j++) {
+      if(i<=half){
+        if( j <= half-i  || j >= half+i   ){
+          star += ' '
+        }else{
+          star += '*'
+        }
+      }
+      if(i>half){
+        if( j <= Math.abs(half-i)  || j >  num- Math.abs(half-i)  ){
+          star += ' '
+        }else{
+          star += '*'
+        }
+      }
     }
     console.log(star);
   }
 }
 
 
-makeStar(3);
-makeStar(5);
-makeStar(10);
+makeStar(7);
